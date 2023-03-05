8cc448dbed6d9e038bcab20ccb08f1ec0f099ca9
Anne <steamengine.a@gmail.com>
2023-03-04 19:06:07 -0500

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,6 @@ INSERT INTO appointment(name, classname, appointmentDate, appointmentTime) VALUE
 ('Sarika P.', 'Ladder barrel', '2022-12-01', '12:00:00'),
 ('Savannah A.', 'Reformer 2', '2022-12-01', '02:00:00'),
 ('Lily A.', 'Reformer 1', '2022-12-01', '03:00:00'),
-('Shyla K.', 'Mat pilates', '2022-12-02', '11:00:00'),
-('Savannah A.', 'Reformer 2', '2022-12-02', '02:00:00'),
-('Jennie K.', 'Ladder barrel', '2022-12-02', '03:00:00'),
+('Shyla K.', 'Mat pilates', '2022-12-02', '02:00:00'),
+('Savannah A.', 'Reformer 2', '2022-12-02', '03:00:00'),
 ('Anna W.', 'Hot yoga ', '2022-12-02', '07:00:00');
